af1e6d2041af7489263a53007f8ce5b01abc4cc2
Atif Afzal <atif5801@gmail.com>
2019-03-21 15:10:17 +0530

    Add server scaffolding

---
+++ server/fuse.server.ts
@@ -0,0 +1,43 @@
+/**
+ * This file handle the main part of the electron app
+ */
+
+import { ChildProcess, spawn } from 'child_process';
+import { FuseBox } from 'fuse-box';
+
+const fuse = FuseBox.init({
+  homeDir: 'src/',
+  output: 'dist/$name.js',
+  sourceMaps: true,
+});
+
+let electronProcess: ChildProcess;
+
+fuse
+  .bundle('server')
+  .target('electron')
+  .instructions(' > [index.ts]')
+  .watch('**')
+  .completed(() => {
+    if (electronProcess && electronProcess.pid) {
+      process.kill(-electronProcess.pid, 'SIGKILL');
+    }
+
+    electronProcess = spawn('electron', [__dirname], {
+      stdio: 'inherit',
+      detached: true,
+    }).on('exit', code => {
+      if (code !== null) {
+        // tslint:disable-next-line:no-console
+        console.log(`electron process exited with code ${code}`);
+
+        process.exit(code);
+      }
+    });
+  });
+
+process.on('SIGINT', () => {
+  process.kill(-electronProcess.pid);
+});
+
+fuse.run();
---
+++ server/src/index.ts
@@ -0,0 +1,46 @@
+import * as electron from 'electron';
+
+let mainWindow: electron.BrowserWindow | null;
+
+function createWindow() {
+  mainWindow = new electron.BrowserWindow({
+    width: 1280,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+    },
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadURL('https://github.com');
+
+  mainWindow.setTitle('Beragi');
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+electron.app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+electron.app.on('window-all-closed', () => {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    electron.app.quit();
+  }
+});
+
+electron.app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
