502acb2be4d2b081d688e21e70a1b96398f7e758
Atif Afzal <atif5801@gmail.com>
2019-04-11 02:15:16 +0530

    Add newIdentifierCreator

---
+++ server/#/utils/tsNode/getNewIdentifierName/index.ts
@@ -0,0 +1,35 @@
+const incrementName = (name: string) => {
+  const regArr = /_\$(\d*)$/.exec(name);
+
+  if (!regArr) {
+    return `${name}_$`;
+  }
+
+  const n = Number(regArr[1]);
+
+  const index = regArr.index;
+
+  return `${name.slice(0, index)}_$${n + 1}`;
+};
+
+const getNewIdentifierNameCreator = (declarationIdentifierNodes: string[]) => {
+  const newNames: Record<string, string> = {};
+
+  return (name: string) => {
+    if (newNames[name]) {
+      return newNames[name];
+    }
+
+    let expectedNewName = incrementName(name);
+
+    while (declarationIdentifierNodes.includes(expectedNewName)) {
+      expectedNewName = incrementName(expectedNewName);
+    }
+
+    newNames[name] = expectedNewName;
+
+    return expectedNewName;
+  };
+};
+
+export default getNewIdentifierNameCreator;
