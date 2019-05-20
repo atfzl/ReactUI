bf4ccd5317620c46a569c11d349454fee6d34913
Atif Afzal <atif5801@gmail.com>
2019-05-20 22:17:30 +0530

    Create Editor route

---
+++ client/#/routes/Editor/index.tsx
@@ -0,0 +1,10 @@
+import Canvas from '#/routes/Editor/components/Canvas';
+import * as React from 'react';
+
+class Editor extends React.Component {
+  public render() {
+    return <Canvas />;
+  }
+}
+
+export default Editor;
--- client/#/routes/index.tsx
+++ client/#/routes/index.tsx
@@ -1,11 +1,11 @@
+import Editor from '#/routes/Editor';
 import * as React from 'react';
 import { Route } from 'react-router-dom';
-import Canvas from './Canvas';
 
 const Routes = () => {
   return (
     <>
-      <Route exact path="/" component={Canvas} />
+      <Route exact path="/" component={Editor} />
     </>
   );
 };
