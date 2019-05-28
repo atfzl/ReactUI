75adae7db7c461088c61acce40550154fc49e41a
Atif Afzal <atif5801@gmail.com>
2019-05-28 16:09:28 +0530

    Setup mocks for testing server code with ipc

---
+++ server/#/__mocks__/electron-better-ipc/source/main/index.ts
@@ -0,0 +1,2 @@
+export const callRenderer = () => Promise.resolve({});
+export const answerMain = () => Promise.resolve({});
--- server/#/services/copyElement/handleChildElements.ts
+++ server/#/services/copyElement/handleChildElements.ts
@@ -1,14 +1,24 @@
+import GetRuntimeProps, {
+  isReactElementIdentifier,
+} from '#/common/api/GetRuntimeProps';
+import { TagCursor } from '#/common/models/file';
 import { isInterinsicTag } from '#/common/utils';
 import { ReplacementBuilder } from '#/utils/ReplacementBuilder';
-import { getTagName, traverseNodeReferences } from '#/utils/tsNode';
+import {
+  getAttributes,
+  getTagName,
+  traverseNodeReferences,
+} from '#/utils/tsNode';
+import * as electron from 'electron';
 import { EMPTY } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import * as ts from 'typescript';
 
 const handleChildElements = (
   rb: ReplacementBuilder,
   sourceFileDeclarationIdentifiers: ts.BindingName[],
   incrementIdentifierName: (s: string) => string,
-  //  cursor: TagCursor,
+  cursor: TagCursor,
 ) => (elementNode: ts.JsxChild) => {
   // TODO: handle else
   if (ts.isJsxElement(elementNode) || ts.isJsxSelfClosingElement(elementNode)) {
@@ -20,56 +30,51 @@ const handleChildElements = (
         rb.replaceNodeWithText(tag, incrementIdentifierName(tag.getText()));
       });
 
-      return traverseNodeReferences(
-        openingTag,
-        sourceFileDeclarationIdentifiers,
+      const win = electron.BrowserWindow.getFocusedWindow()!;
+
+      return GetRuntimeProps.callRenderer(win, cursor).pipe(
+        switchMap(props => {
+          const newProps: string[] = [];
+
+          Object.keys(props).forEach(prop => {
+            const value = props[prop];
+
+            if (prop === 'children') {
+              return;
+            }
+
+            let newValue = '';
+            switch (true) {
+              case value === isReactElementIdentifier:
+                newValue = '{null}';
+                break;
+              case typeof value === 'string':
+                newValue = `"${value}"`;
+                break;
+              case typeof value === 'number':
+                newValue = `{${value}}`;
+                break;
+              default:
+                newValue = JSON.stringify(value);
+            }
+
+            newProps.push(`${prop}=${newValue}`);
+          });
+
+          if (newProps.length) {
+            const newPropsText = newProps.join('\n');
+
+            const attributes = getAttributes(elementNode);
+
+            rb.replaceNodeWithText(attributes, newPropsText);
+          }
+
+          return traverseNodeReferences(
+            openingTag,
+            sourceFileDeclarationIdentifiers,
+          );
+        }),
       );
-
-      // const win = electron.BrowserWindow.getFocusedWindow()!;
-
-      // return GetRuntimeProps.callRenderer(win, cursor).pipe(
-      //   switchMap(props => {
-      //     const newProps: string[] = [];
-
-      //     Object.keys(props).forEach(prop => {
-      //       const value = props[prop];
-
-      //       if (prop === 'children') {
-      //         return;
-      //       }
-
-      //       let newValue = '';
-      //       switch (true) {
-      //         case value === isReactElementIdentifier:
-      //           newValue = '{null}';
-      //           break;
-      //         case typeof value === 'string':
-      //           newValue = `"${value}"`;
-      //           break;
-      //         case typeof value === 'number':
-      //           newValue = `{${value}}`;
-      //           break;
-      //         default:
-      //           newValue = JSON.stringify(value);
-      //       }
-
-      //       newProps.push(`${prop}=${newValue}`);
-      //     });
-
-      //     if (newProps.length) {
-      //       const newPropsText = newProps.join('\n');
-
-      //       const attributes = getAttributes(elementNode);
-
-      //       rb.replaceNodeWithText(attributes, newPropsText);
-      //     }
-
-      //     return traverseNodeReferences(
-      //       openingTag,
-      //       sourceFileDeclarationIdentifiers,
-      //     );
-      //   }),
-      // );
     }
   }
 
