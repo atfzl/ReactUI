0496c2a7e6662e65da2a4265f97db027d97b2f68
Atif Afzal <atif5801@gmail.com>
2019-05-31 21:04:16 +0530

    Separate Gallery frame as separate component from RightPanel

---
+++ client/#/routes/Editor/containers/GalleryFrame/index.tsx
@@ -0,0 +1,150 @@
+import AutoScale from '#/components/AutoScale';
+import Isolate from '#/components/Isolate';
+import { RootState } from '#/reducers';
+import actions from '#/reducers/gallery/actions';
+import { Theme } from '#/styled/theme';
+import { withTheme } from 'emotion-theming';
+import * as React from 'react';
+import * as ReactDOMServer from 'react-dom/server';
+import { connect } from 'react-redux';
+import { pipe } from 'rxjs';
+
+const ONE_SIDE_PADDING = 24;
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps;
+
+interface Props extends StateProps, DispatchProps {
+  theme: Theme;
+  width: number;
+}
+
+class GalleryFrame extends React.PureComponent<Props> {
+  private instanceWrappers: HTMLDivElement[] = [];
+
+  private onFrameReady = ({
+    doc,
+    element,
+  }: {
+    doc: Document;
+    element: HTMLDivElement;
+  }) => {
+    this.props.setCanvasInternals({ doc, element });
+  };
+
+  private collectRef = (index: number) => (element: HTMLDivElement) => {
+    this.instanceWrappers[index] = element;
+  };
+
+  public render() {
+    const {
+      workspace,
+      selectedComponent,
+      setSelectedComponent,
+      theme,
+      width,
+    } = this.props;
+
+    let index = 0;
+
+    return (
+      <Isolate
+        onReady={this.onFrameReady}
+        style={{
+          height: '100%',
+          width,
+        }}
+        wrapperStyle={{
+          paddingLeft: ONE_SIDE_PADDING,
+          paddingRight: ONE_SIDE_PADDING,
+        }}
+        injectCSS={
+          "@import url('https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,600&display=swap');"
+        }
+      >
+        {workspace && (
+          <div>
+            {workspace.components.map((component, i) => (
+              <div style={{ marginTop: 20, marginBottom: 32 }} key={i}>
+                <div
+                  style={{
+                    fontFamily: 'IBM Plex Sans, sans-serif',
+                    fontSize: 16,
+                    marginLeft: -8,
+                    fontWeight: 600,
+                  }}
+                >
+                  {component.title}
+                </div>
+                <div>
+                  {component.instances.map((instance, j) => (
+                    <div
+                      style={{
+                        marginBottom: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'flex-start',
+                      }}
+                      key={j}
+                    >
+                      <div
+                        style={{
+                          fontFamily: 'IBM Plex Sans, sans-serif',
+                          fontSize: 16,
+                          marginBottom: 5,
+                          alignSelf: 'flex-end',
+                          borderBottom:
+                            selectedComponent[0] === i &&
+                            selectedComponent[1] === j
+                              ? `2px solid ${theme.colors.primary}`
+                              : '2px solid transparent',
+                          cursor: 'pointer',
+                        }}
+                        onClick={() => setSelectedComponent([i, j])}
+                      >
+                        {instance.title}
+                      </div>
+                      <AutoScale maxWidth={width - ONE_SIDE_PADDING * 2}>
+                        <div
+                          style={{
+                            display: 'inline-block',
+                            border: '1px solid rgba(0, 0, 0, 0.3)',
+                            userSelect: 'none',
+                          }}
+                          ref={this.collectRef(index++)}
+                          dangerouslySetInnerHTML={{
+                            __html: ReactDOMServer.renderToStaticMarkup(
+                              instance.element,
+                            ),
+                          }}
+                        />
+                      </AutoScale>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </Isolate>
+    );
+  }
+}
+
+const mapStateToProps = (state: RootState) => ({
+  workspace: state.gallery.workspace,
+  selectedComponent: state.gallery.selectedComponent,
+});
+
+const mapDispatchToProps = {
+  setCanvasInternals: actions.setCanvasInternals,
+  setSelectedComponent: actions.setSelectedComponent,
+};
+
+export default pipe(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps,
+  ),
+  withTheme,
+)(GalleryFrame);
--- client/#/routes/Editor/containers/RightPanel/index.tsx
+++ client/#/routes/Editor/containers/RightPanel/index.tsx
@@ -1,49 +1,23 @@
-import AutoScale from '#/components/AutoScale';
-import Isolate from '#/components/Isolate';
-import { RootState } from '#/reducers';
-import actions from '#/reducers/gallery/actions';
-import styled from '#/styled';
-import { Theme } from '#/styled/theme';
-import { withTheme } from 'emotion-theming';
+import GalleryFrame from '#/routes/Editor/containers/GalleryFrame';
+import styled from '@emotion/styled';
 import * as React from 'react';
-import * as ReactDOMServer from 'react-dom/server';
-import { connect } from 'react-redux';
-import { pipe } from 'rxjs';
-import { oc } from 'ts-optchain';
-
-const ONE_SIDE_PADDING = 24;
 
 const Container = styled.div`
   background-color: ${props => props.theme.colors.background[100]};
   height: 100vh;
 `;
 
-type StateProps = ReturnType<typeof mapStateToProps>;
-type DispatchProps = typeof mapDispatchToProps;
-
-interface Props extends StateProps, DispatchProps {
-  theme: Theme;
-}
-
 interface State {
   containerRect?: ClientRect;
 }
 
-class RightPanel extends React.PureComponent<Props, State> {
+class RightPanel extends React.PureComponent<{}, State> {
   public state: State = {};
 
   private containerRef = React.createRef<HTMLDivElement>();
   private resizeObserver: any;
 
-  private onFrameReady = ({
-    doc,
-    element,
-  }: {
-    doc: Document;
-    element: HTMLDivElement;
-  }) => {
-    this.props.setCanvasInternals({ doc, element });
-
+  public componentDidMount() {
     this.resizeObserver = new (window as any).ResizeObserver((entries: any) => {
       for (const entry of entries) {
         this.setState({ containerRect: entry.contentRect });
@@ -51,7 +25,7 @@ class RightPanel extends React.PureComponent<Props, State> {
     });
 
     this.resizeObserver.observe(this.containerRef.current);
-  };
+  }
 
   public componentWillUnmount() {
     if (this.resizeObserver) {
@@ -60,117 +34,14 @@ class RightPanel extends React.PureComponent<Props, State> {
   }
 
   public render() {
-    const {
-      workspace,
-      selectedComponent,
-      setSelectedComponent,
-      theme,
-    } = this.props;
-
     return (
       <Container ref={this.containerRef}>
-        <Isolate
-          onReady={this.onFrameReady}
-          style={{
-            height: '100%',
-            width: oc(this.state.containerRect).width(),
-          }}
-          wrapperStyle={{
-            paddingLeft: ONE_SIDE_PADDING,
-            paddingRight: ONE_SIDE_PADDING,
-          }}
-          injectCSS={
-            "@import url('https://fonts.googleapis.com/css?family=IBM+Plex+Sans:400,600&display=swap');"
-          }
-        >
-          {workspace && this.state.containerRect && (
-            <div>
-              {workspace.components.map((component, i) => (
-                <div style={{ marginTop: 20, marginBottom: 32 }} key={i}>
-                  <div
-                    style={{
-                      fontFamily: 'IBM Plex Sans, sans-serif',
-                      fontSize: 16,
-                      marginLeft: -8,
-                      fontWeight: 600,
-                    }}
-                  >
-                    {component.title}
-                  </div>
-                  <div>
-                    {component.instances.map((instance, j) => (
-                      <div
-                        style={{
-                          marginBottom: 8,
-                          display: 'flex',
-                          flexDirection: 'column',
-                          alignItems: 'flex-start',
-                        }}
-                        key={j}
-                      >
-                        <div
-                          style={{
-                            fontFamily: 'IBM Plex Sans, sans-serif',
-                            fontSize: 16,
-                            marginBottom: 5,
-                            alignSelf: 'flex-end',
-                            borderBottom:
-                              selectedComponent[0] === i &&
-                              selectedComponent[1] === j
-                                ? `2px solid ${theme.colors.primary}`
-                                : '2px solid transparent',
-                            cursor: 'pointer',
-                          }}
-                          onClick={() => setSelectedComponent([i, j])}
-                        >
-                          {instance.title}
-                        </div>
-                        <AutoScale
-                          maxWidth={
-                            this.state.containerRect!.width -
-                            ONE_SIDE_PADDING * 2
-                          }
-                        >
-                          <div
-                            style={{
-                              display: 'inline-block',
-                              border: '1px solid rgba(0, 0, 0, 0.3)',
-                              userSelect: 'none',
-                            }}
-                            dangerouslySetInnerHTML={{
-                              __html: ReactDOMServer.renderToStaticMarkup(
-                                instance.element,
-                              ),
-                            }}
-                          />
-                        </AutoScale>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              ))}
-            </div>
-          )}
-        </Isolate>
+        {this.state.containerRect && (
+          <GalleryFrame width={this.state.containerRect.width} />
+        )}
       </Container>
     );
   }
 }
 
-const mapStateToProps = (state: RootState) => ({
-  workspace: state.gallery.workspace,
-  selectedComponent: state.gallery.selectedComponent,
-});
-
-const mapDispatchToProps = {
-  setCanvasInternals: actions.setCanvasInternals,
-  setSelectedComponent: actions.setSelectedComponent,
-};
-
-export default pipe(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps,
-  ),
-  withTheme,
-)(RightPanel);
+export default RightPanel;
