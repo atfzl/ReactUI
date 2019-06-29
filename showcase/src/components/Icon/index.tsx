3a7e4d65dea49802419a5b78266696b6af46aa19
Atif Afzal <atif5801@gmail.com>
2019-06-29 23:45:39 +0530

    create augli ui playground

--- showcase/src/components/Icon/index.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const Icon = styled.i.attrs({
-  className: 'material-icons',
-})``;
-
-export default Icon;
---
+++ showcase/src/components/mainScreen/index.tsx
@@ -0,0 +1,138 @@
+import * as React from 'react';
+import {
+  default as styled,
+  default as styled_$,
+  default as styled_$1,
+} from 'styled-components';
+
+const Div_$16 = styled.div`
+  font-size: 15px;
+`;
+
+const Div_$15 = styled.div`
+  font-size: 15px;
+`;
+
+const Div_$14 = styled.div`
+  color: white;
+  margin-left: 20px;
+`;
+
+const Div_$13 = styled.div`
+  height: 20px;
+  width: 20px;
+  background-color: lightgrey;
+  border: 1px solid black;
+`;
+
+const Div_$12 = styled.div`
+  display: flex;
+`;
+
+const Div_$11 = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Div_$10 = styled_$1.div`
+height: 10px;
+width: 10px;
+background-color: white;
+margin-left: 16px;
+`;
+const Div_$9 = styled_$.div`
+height: 10px;
+width: 10px;
+background-color: white;
+`;
+const Div_$8 = styled.div`
+  height: 10px;
+  width: 10px;
+  background-color: white;
+  margin-left: 16px;
+`;
+
+const Div_$7 = styled.div`
+  display: flex;
+`;
+
+const Div_$6 = styled.div`
+  height: 200px;
+  width: 100%;
+  background-color: lightgrey;
+  border: 1px solid black;
+`;
+
+const Div_$5 = styled.div`
+  background-color: green;
+  width: 20px;
+  height: 20px;
+`;
+
+const Div_$4 = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Div_$3 = styled.div`
+  height: 267px;
+  width: 100%;
+  background-image: linear-gradient(151deg, #3f28ac, #466af1);
+  padding: 20px;
+  padding-top: 46px;
+`;
+
+const Div_$2 = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: lightgrey;
+  border: 1px solid black;
+`;
+
+const Div_$1 = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: lightgrey;
+  border: 1px solid black;
+`;
+
+const Div_$ = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: lightgrey;
+  border: 1px solid black;
+`;
+
+const Container = styled.div`
+  all: unset;
+`;
+
+class MainScreen extends React.Component {
+  public render() {
+    return (
+      <Container>
+        <Div_$3>
+          <Div_$4>
+            <Div_$5 />
+            <Div_$7>
+              <Div_$8 />
+              <Div_$10 />
+            </Div_$7>
+          </Div_$4>
+          <Div_$11>
+            <Div_$12>
+              <Div_$13 />
+              <Div_$14>
+                <Div_$15>Scribbler</Div_$15>
+                <Div_$16>Level 4</Div_$16>
+              </Div_$14>
+            </Div_$12>
+          </Div_$11>
+        </Div_$3>
+        <Div_$6 />
+      </Container>
+    );
+  }
+}
+
+export default MainScreen;
