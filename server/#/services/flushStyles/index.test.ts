d29d51e7260dffcd5f6c267763c217244ec293c1
Atif Afzal <atif5801@gmail.com>
2019-06-29 18:25:49 +0530

    Update old tests

--- server/#/services/flushStyles/index.test.ts
+++ server/#/services/flushStyles/index.test.ts
@@ -4,7 +4,7 @@ describe('no substitution template literal', () => {
   const fileNoSubstitutionTemplate1 = `${__dirname}/__fixtures__/noSubstitutionTemplate1.tsx`;
 
   it('success', done => {
-    flushStyles$({ display: 'flex' })({
+    flushStyles$([{ key: 'display', value: 'flex' }])({
       fileName: fileNoSubstitutionTemplate1,
       lineNumber: 3,
       columnNumber: 13,
@@ -19,7 +19,7 @@ describe('substitution template literal', () => {
   const fileSubstitutionTemplate1 = `${__dirname}/__fixtures__/substitutionTemplate1.tsx`;
 
   it('should override static string in tail of last template', done => {
-    flushStyles$({ display: 'flex' })({
+    flushStyles$([{ key: 'display', value: 'flex' }])({
       fileName: fileSubstitutionTemplate1,
       lineNumber: 3,
       columnNumber: 13,
