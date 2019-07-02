725afacac0a0eb0faf564a21ff0f010c847cc193
Atif Afzal <atif5801@gmail.com>
2019-07-02 21:09:32 +0530

    Revert "create augli ui playground"
    This reverts commit 3a7e4d65dea49802419a5b78266696b6af46aa19.

---
+++ showcase/src/components/StatusBar/index.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import Icon from '../Icon';
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 4px 6px;
+  color: grey;
+  font-size: 13px;
+`;
+
+const Text = styled.div``;
+
+const BatteryPercentageText = styled(Text)`
+  margin-left: 4px;
+`;
+
+const MaterialIcon = styled(Icon)`
+  font-size: inherit;
+`;
+
+const RightIconsRowContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const StatusBar = () => (
+  <Container>
+    <div>
+      <Text>12:26</Text>
+    </div>
+    <RightIconsRowContainer>
+      <MaterialIcon>location_on</MaterialIcon>
+      <MaterialIcon>alarm</MaterialIcon>
+      <MaterialIcon>bluetooth_connected</MaterialIcon>
+      <MaterialIcon>signal_cellular_null</MaterialIcon>
+      <BatteryPercentageText>98%</BatteryPercentageText>
+      <MaterialIcon>battery_full</MaterialIcon>
+    </RightIconsRowContainer>
+  </Container>
+);
+
+export default StatusBar;
--- showcase/src/reactui.tsx
+++ showcase/src/reactui.tsx
@@ -1,14 +1,11 @@
-import styled from 'styled-components'
 import * as React from 'react';
 import { injectGlobal } from 'styled-components';
-import MainScreen from './components/mainScreen';const Div_$ = styled.div`
-                 height: 200px;
-                 width: 200px;
-                 background-color: lightgrey;
-                 border: 1px solid black;
-             `;
-              
-
+import AlbumArt from './components/AlbumArt';
+import Page from './components/Page';
+import Seeker from './components/Seeker';
+import PlayControl from './components/PlayControl';
+import StatusBar from './components/StatusBar';
+import Icon from './components/Icon';
 
 // tslint:disable-next-line no-unused-expression
 injectGlobal`
@@ -26,15 +23,68 @@ body {
 
 export const components = [
   {
-    title: 'Main Screen',
+    title: 'Page',
     instances: [
       {
-        title: 'default',
+        title: 'main',
+        element: <Page />,
+      },
+    ],
+  },
+  {
+    title: 'Play Control',
+    instances: [
+      {
+        title: 'main',
+        element: <PlayControl />,
+      },
+    ],
+  },
+  {
+    title: 'Status Bar',
+    instances: [
+      {
+        title: 'main',
+        element: <StatusBar />,
+      },
+    ],
+  },
+  {
+    title: 'Icon',
+    instances: [
+      {
+        title: 'close',
+        element: <Icon>close</Icon>,
+      },
+    ],
+  },
+  {
+    title: 'Album Art',
+    instances: [
+      {
+        title: 'opus',
         element: (
-          <div style={{ width: 375 }}>
-            <MainScreen />
+          <div style={{ width: 200, height: 200 }}>
+            <AlbumArt src="https://upload.wikimedia.org/wikipedia/en/6/6e/Opus_Eric_Prydz_cover_artwork.jpg" />
           </div>
         ),
+      },
+      {
+        title: 'against all logic',
+        element: (
+          <div style={{ width: 200, height: 200 }}>
+            <AlbumArt src="https://media.pitchfork.com/photos/5a95784ab848c0268b200ffd/1:1/w_320/2012%20-%202017.jpg" />
+          </div>
+        ),
+      },
+    ],
+  },
+  {
+    title: 'Seeker',
+    instances: [
+      {
+        title: 'main',
+        element: <Seeker currentTime={108} endTime={671} />,
       },
     ],
   },
